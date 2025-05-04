fdd9ba5808c5f3bb1caba0faa9080676d611e0c4
DavidAntolinez <antolinezchavarrodavid@gmail.com>
2025-05-04 16:06:49 -0500

    diseño frontend

--- src/app/client/dashboard/page.tsx
+++ src/app/client/dashboard/page.tsx
@@ -2,9 +2,10 @@
 
 import { Badge } from "@/components/ui/badge"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import {usePrivy} from '@privy-io/react-auth';
+import { usePrivy } from '@privy-io/react-auth';
 import { useEffect, useState } from 'react';
 import './styles.css';
+import Loading from "../components/Loanding";
 
 interface Equipment {
   id: string;
@@ -17,60 +18,105 @@ interface Equipment {
 export default function EquipmentDashboard() {
   const [equipments, setEquipments] = useState<Equipment[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(true); // Estado para verificar si el usuario es admin
+  const [adminChecking, setAdminChecking] = useState(true); // Estado para controlar la carga de la verificación
   const { login, logout, authenticated, user } = usePrivy();
 
   useEffect(() => {
     if (authenticated) {
-      fetchEquipmentData();
+      checkAdminStatus();
     }
   }, [authenticated]);
+
+  useEffect(() => {
+    if (authenticated && isAdmin) {
+      fetchEquipmentData();
+    }
+  }, [authenticated, isAdmin]);
+
+  // Función para verificar si el usuario es administrador
+  const checkAdminStatus = async () => {
+    try {
+      setAdminChecking(true);
+
+      // Realizar petición al backend para verificar si es admin
+      const response = await fetch('/api/checkAdmin', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setIsAdmin(data.isAdmin); // data.isAdmin será true o false según la respuesta del backend
+      } else {
+        setIsAdmin(false);
+      }
+    } catch (error) {
+      console.error('Error checking admin status:', error);
+      setIsAdmin(true);
+    } finally {
+      setAdminChecking(false);
+    }
+  };
 
   const fetchEquipmentData = async () => {
     try {
       setIsLoading(true);
       // Simulando una llamada a la API con un retraso de 1 segundo
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      
-      // Datos placeholder
-      const placeholderData: Equipment[] = [
-        {
-          id: "7e0efe28e710a775596d3b93c8c26509",
-          serial: "NHQ59AL00H9480D4F83400",
-          os: "NixOS",
-          pendingUpdates: 13,
-          status: "Actualizando",
-        },
-        {
-          id: "7e0efe28e710a775596d3b93c8c26510",
-          serial: "NHQ59AL00H9480D4F83401",
-          os: "Ubuntu",
-          pendingUpdates: 2,
-          status: "Actualizando",
-        },
-        {
-          id: "7e0efe28e710a775596d3b93c8c26511",
-          serial: "NHQ59AL00H9480D4F83402",
-          os: "Alpine",
-          pendingUpdates: 9,
-          status: "Actualizando",
-        },
-        {
-          id: "7e0efe28e710a775596d3b93c8c26512",
-          serial: "NHQ59AL00H9480D4F83403",
-          os: "Manjaro",
-          pendingUpdates: 2,
-          status: "Actualizando",
-        },
-        {
-          id: "7e0efe28e710a775596d3b93c8c26513",
-          serial: "NHQ59AL00H9480D4F83404",
-          os: "NixOS",
-          pendingUpdates: 13,
-          status: "Actualizando",
-        },
-      ];
+      const response = await fetch('/api/getData', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setEquipments(data);
+      } else {
 
-      setEquipments(placeholderData);
+
+        // Datos placeholder
+        const placeholderData: Equipment[] = [
+          {
+            id: "7e0efe28e710a775596d3b93c8c26509",
+            serial: "NHQ59AL00H9480D4F83400",
+            os: "NixOS",
+            pendingUpdates: 13,
+            status: "Actualizando",
+          },
+          {
+            id: "7e0efe28e710a775596d3b93c8c26510",
+            serial: "NHQ59AL00H9480D4F83401",
+            os: "Ubuntu",
+            pendingUpdates: 2,
+            status: "Actualizando",
+          },
+          {
+            id: "7e0efe28e710a775596d3b93c8c26511",
+            serial: "NHQ59AL00H9480D4F83402",
+            os: "Alpine",
+            pendingUpdates: 9,
+            status: "Actualizando",
+          },
+          {
+            id: "7e0efe28e710a775596d3b93c8c26512",
+            serial: "NHQ59AL00H9480D4F83403",
+            os: "Manjaro",
+            pendingUpdates: 2,
+            status: "Actualizando",
+          },
+          {
+            id: "7e0efe28e710a775596d3b93c8c26513",
+            serial: "NHQ59AL00H9480D4F83404",
+            os: "NixOS",
+            pendingUpdates: 13,
+            status: "Actualizando",
+          },
+        ];
+        setEquipments(placeholderData);
+      }
     } catch (error) {
       console.error('Error fetching equipment data:', error);
     } finally {
@@ -78,8 +124,36 @@ export default function EquipmentDashboard() {
     }
   };
 
+  // Si no está autenticado, mostrar botón de login
   if (!authenticated) {
-    return <button onClick={login}>Conectar Wallet</button>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <button onClick={login} className="btn">Conectar Wallet</button>
+      </div>
+    );
+  }
+
+  // Si está autenticado pero está verificando si es admin
+  if (adminChecking) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-pulse text-white">
+          Verificando permisos...
+        </div>
+      </div>
+    );
+  }
+
+  // Si está autenticado pero no es admin
+  if (!isAdmin) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <div className="text-white text-xl">
+          No tienes permisos de administrador para acceder a esta página.
+        </div>
+        <button onClick={logout} className="btn">Desconectar</button>
+      </div>
+    );
   }
 
   const address = user?.wallet?.address;
@@ -89,6 +163,7 @@ export default function EquipmentDashboard() {
     return "green"
   }
 
+  // Si está autenticado y es admin, mostrar el dashboard
   return (
     <div className="p-6 max-w-full">
       {/* Contenedor de conexión y botones */}
@@ -98,38 +173,34 @@ export default function EquipmentDashboard() {
           <button onClick={logout} className="btn">Desconectar</button>
         </div>
       </div>
-    
+
       {/* Contenedor de la tabla */}
       <div className="table-container">
-        <h1 className="text-4xl font-bold mb-4">Estado de los equipos</h1>
-        
+        <h1 className="text-4xl font-bold mb-4 text-white">Estado de los equipos</h1>
+
         {isLoading ? (
-          <div className="flex justify-center items-center h-64">
-            <div className="animate-pulse text-gray-500">
-              Cargando datos...
-            </div>
-          </div>
+          <Loading />
         ) : (
-          <Table className="custom-table">
+          <Table className="custom-table border-solid" >
             <TableHeader>
               <TableRow>
-                <TableHead className="font-medium">ID del equipo</TableHead>
-                <TableHead className="font-medium">Número de serial</TableHead>
-                <TableHead className="font-medium">Sistema operativo</TableHead>
-                <TableHead className="font-medium">Estado actualizaciones</TableHead>
-                <TableHead className="font-medium">Estado</TableHead>
+                <TableHead className="font-medium text-white">ID del equipo</TableHead>
+                <TableHead className="font-medium text-white">Número de serial</TableHead>
+                <TableHead className="font-medium text-white">Sistema operativo</TableHead>
+                <TableHead className="font-medium text-white">Estado actualizaciones</TableHead>
+                <TableHead className="font-medium text-white">Estado</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {equipments.map((equipment) => (
                 <TableRow key={equipment.id}>
-                  <TableCell className="font-mono">{equipment.id}</TableCell>
-                  <TableCell className="font-mono">{equipment.serial}</TableCell>
-                  <TableCell className="font-mono">{equipment.os}</TableCell>
+                  <TableCell className="font-mono text-white">{equipment.id}</TableCell>
+                  <TableCell className="font-mono text-white">{equipment.serial}</TableCell>
+                  <TableCell className="font-mono text-white">{equipment.os}</TableCell>
                   <TableCell>
                     <Badge
                       variant="outline"
-                      className={`bg-${getUpdateBadgeVariant(equipment.pendingUpdates)}-400 text-black border-none px-3 py-1 font-mono`}
+                      className={`bg-${getUpdateBadgeVariant(equipment.pendingUpdates)}-400 text-white border-none px-3 py-1 font-mono`}
                     >
                       {equipment.pendingUpdates} actualizaciones pendientes
                     </Badge>
@@ -137,7 +208,7 @@ export default function EquipmentDashboard() {
                   <TableCell>
                     <Badge
                       variant="outline"
-                      className={`bg-${equipment.status.toLowerCase() === "actualizando" ? "amber" : "green"}-400 hover:bg-${equipment.status.toLowerCase() === "actualizando" ? "amber" : "green"}-500 text-black border-none font-mono px-3 py-1`}
+                      className={`bg-${equipment.status.toLowerCase() === "actualizando" ? "amber" : "green"}-400 hover:bg-${equipment.status.toLowerCase() === "actualizando" ? "amber" : "green"}-500 text-white border-none font-mono px-3 py-1`}
                     >
                       {equipment.status}
                     </Badge>
@@ -147,7 +218,7 @@ export default function EquipmentDashboard() {
             </TableBody>
           </Table>
         )}
-        <p>Cuenta como Administrador</p>
+        <p className="text-white">Cuenta como Administrador</p>
       </div>
     </div>
   )
--- src/app/client/details/page.tsx
+++ src/app/client/details/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import "./style.css";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import Loading from "../components/Loanding";
 
 interface SoftwareVersion {
   name: string
@@ -21,7 +22,10 @@ interface DeviceInfo {
 }
 
 export default function DeviceInfoPage() {
-  const [deviceInfo] = useState<DeviceInfo>({
+  const [isUser, setIsUser] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // Estado para verificar si el usuario es User (comienza como falso)
+  const [userChecking, setUserChecking] = useState(true); // Estado para controlar la carga de la verificación
+  const [deviceInfo,setDeviceInfo] = useState<DeviceInfo>({
     id: "7e0efe28e710a775596d3b93c8c26509",
     serialNumber: "NHQ59AL00H9480D4F83400",
     operatingSystem: "NixOS",
@@ -33,89 +37,175 @@ export default function DeviceInfoPage() {
       { name: "chromium", currentVersion: "3.4.5", latestVersion: "4.0.1" },
     ],
     isUpdating: true,
-  })
+  });
+
+  useEffect(() => {
+    checkUserStatus();
+  }, []);  
+
+  useEffect(() => {
+    if (isUser) {
+      fetchData();
+    }
+  }, [isUser]);
+
+  const checkUserStatus = async () => {
+    try {
+      setUserChecking(true);
+
+      // Realizar petición al backend para verificar si es User
+      const response = await fetch('/api/checkUser', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setIsUser(data.isUser); // data.isUser será true o false según la respuesta del backend
+      } else {
+        setIsUser(true);
+      }
+    } catch (error) {
+      console.error('Error checking User status:', error);
+      setIsUser(true);
+    } finally {
+      setUserChecking(false);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      // Simulando una llamada a la API con un retraso de 1 segundo
+      const response = await fetch('/api/getPcData', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setDeviceInfo(data)
+      } else {
+
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Si está verificando si es usuario
+  if (userChecking) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-pulse">
+          Verificando permisos...
+        </div>
+      </div>
+    );
+  }
+
+  // Si no es usuario
+  if (!isUser) {
+    return (
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <div className="text-xl">
+          No tienes permisos de usuario para acceder a esta página.
+        </div>
+        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Volver
+        </button>
+      </div>
+    );
+  }
 
   const pendingUpdatesCount = deviceInfo.softwareVersions.filter(
-      (software) => software.currentVersion !== software.latestVersion,
+    (software) => software.currentVersion !== software.latestVersion,
   ).length
 
+  // Si es usuario, mostrar la información del dispositivo
   return (
-      <div className="container mx-auto py-10 space-y-6">
-        <h1 className="text-3xl font-bold">Equipo <span className="font-mono">{deviceInfo.serialNumber}</span></h1>
+    <div className="container mx-auto py-10 space-y-6">
+      
+      <h1 className="text-3xl font-bold">Equipo <span className="font-mono">{deviceInfo.serialNumber}</span></h1>
 
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <Card>
-            <CardHeader>
-              <CardTitle>ID del equipo</CardTitle>
-            </CardHeader>
-            <CardContent className="font-mono">
-              <p>{deviceInfo.id}</p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Numero de serial</CardTitle>
-            </CardHeader>
-            <CardContent className="font-mono">
-              <p>{deviceInfo.serialNumber}</p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Sistema operativo</CardTitle>
-            </CardHeader>
-            <CardContent className="font-mono">
-              <p>{deviceInfo.operatingSystem}</p>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader className="flex flex-row align-center justify-between">
-              <CardTitle>Estado</CardTitle>
-              <Badge
-                  variant="warning"
-                  className={`${deviceInfo.isUpdating ? "bg-[#fbbf24]" : "bg-green-500"} hover:bg-[#f59e0b]`}
-              >
-                {deviceInfo.isUpdating ? "Actualizando" : "Actualizado"}
-              </Badge>
-            </CardHeader>
-          </Card>
-        </div>
-
-        {/* Card de actualizaciones que ocupa toda la sección */}
-        <Card className="w-full">
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle>Estado actualizaciones</CardTitle>
-            {pendingUpdatesCount > 0 && (
-                <Badge variant="warning" className="bg-[#fbbf24] hover:bg-[#f59e0b]">
-                  {pendingUpdatesCount} actualizaciones pendientes
-                </Badge>
-            )}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <Card>
+          <CardHeader>
+            <CardTitle>ID del equipo</CardTitle>
           </CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Nombre del programa</TableHead>
-                  <TableHead>Version actual</TableHead>
-                  <TableHead>Version mas reciente</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {deviceInfo.softwareVersions.map((software, index) => (
-                    <TableRow key={software.name}>
-                      <TableCell className="w-full font-mono">{software.name}</TableCell>
-                      <TableCell className="w-1/3 text-right font-mono">{software.currentVersion}</TableCell>
-                      <TableCell className="w-1/3 text-right font-mono">{software.latestVersion}</TableCell>
-                    </TableRow>
-                ))}
-              </TableBody>
-            </Table>
+          <CardContent className="font-mono">
+            <p>{deviceInfo.id}</p>
           </CardContent>
         </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Numero de serial</CardTitle>
+          </CardHeader>
+          <CardContent className="font-mono">
+            <p>{deviceInfo.serialNumber}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Sistema operativo</CardTitle>
+          </CardHeader>
+          <CardContent className="font-mono">
+            <p>{deviceInfo.operatingSystem}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row align-center justify-between">
+            <CardTitle>Estado</CardTitle>
+            <Badge
+              variant="warning"
+              className={`${deviceInfo.isUpdating ? "bg-[#fbbf24]" : "bg-green-500"} hover:bg-[#f59e0b]`}
+            >
+              {deviceInfo.isUpdating ? "Actualizando" : "Actualizado"}
+            </Badge>
+          </CardHeader>
+        </Card>
       </div>
+
+      {/* Card de actualizaciones que ocupa toda la sección */}
+      <Card className="w-full">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle>Estado actualizaciones</CardTitle>
+          {pendingUpdatesCount > 0 && (
+            <Badge variant="warning" className="bg-[#fbbf24] hover:bg-[#f59e0b]">
+              {pendingUpdatesCount} actualizaciones pendientes
+            </Badge>
+          )}
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Nombre del programa</TableHead>
+                <TableHead>Version actual</TableHead>
+                <TableHead>Version mas reciente</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {deviceInfo.softwareVersions.map((software) => (
+                <TableRow key={software.name}>
+                  <TableCell className="w-full font-mono">{software.name}</TableCell>
+                  <TableCell className="w-1/3 text-right font-mono">{software.currentVersion}</TableCell>
+                  <TableCell className="w-1/3 text-right font-mono">{software.latestVersion}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+      
   )
 }
